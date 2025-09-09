7042abd15aba444d418f90a49b599686004d80ee
zhiwzhou3 <delia.zhou@student.unimelb.edu.au>
2025-09-09 22:33:40 +1000

    feat(general): :lipstick: import custom typographies

--- app/theme/typography.ts
+++ app/theme/typography.ts
@@ -1,53 +1,28 @@
 // TODO: write documentation about fonts and typography along with guides on how to add custom fonts in own
 // markdown file and add links from here
 
-import { Platform } from "react-native"
-import {
-  SpaceGrotesk_300Light as spaceGroteskLight,
-  SpaceGrotesk_400Regular as spaceGroteskRegular,
-  SpaceGrotesk_500Medium as spaceGroteskMedium,
-  SpaceGrotesk_600SemiBold as spaceGroteskSemiBold,
-  SpaceGrotesk_700Bold as spaceGroteskBold,
-} from "@expo-google-fonts/space-grotesk"
-
 export const customFontsToLoad = {
-  spaceGroteskLight,
-  spaceGroteskRegular,
-  spaceGroteskMedium,
-  spaceGroteskSemiBold,
-  spaceGroteskBold,
+  "Montserrat": require("../../assets/fonts/Montserrat-VariableFont_wght.ttf"),
+  "Montserrat-Italic": require("../../assets/fonts/Montserrat-Italic-VariableFont_wght.ttf"),
+  "Inter": require("../../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
+  "Inter-Italic": require("../../assets/fonts/Inter-Italic-VariableFont_opsz,wght.ttf"),
 }
 
 const fonts = {
-  spaceGrotesk: {
+  Montserrat: {
     // Cross-platform Google font.
-    light: "spaceGroteskLight",
-    normal: "spaceGroteskRegular",
-    medium: "spaceGroteskMedium",
-    semiBold: "spaceGroteskSemiBold",
-    bold: "spaceGroteskBold",
+    light: "Montserrat-Light",
+    normal: "Montserrat-Regular",
+    medium: "Montserrat-Medium",
+    semiBold: "Montserrat-SemiBold",
+    bold: "Montserrat-Bold",
   },
-  helveticaNeue: {
-    // iOS only font.
-    thin: "HelveticaNeue-Thin",
-    light: "HelveticaNeue-Light",
-    normal: "Helvetica Neue",
-    medium: "HelveticaNeue-Medium",
-  },
-  courier: {
-    // iOS only font.
-    normal: "Courier",
-  },
-  sansSerif: {
-    // Android only font.
-    thin: "sans-serif-thin",
-    light: "sans-serif-light",
-    normal: "sans-serif",
-    medium: "sans-serif-medium",
-  },
-  monospace: {
-    // Android only font.
-    normal: "monospace",
+  Inter: {
+    light: "Inter-Light",
+    normal: "Inter-Regular",
+    medium: "Inter-Medium",
+    semiBold: "Inter-SemiBold",
+    bold: "Inter-Bold",
   },
 }
 
@@ -59,13 +34,9 @@ export const typography = {
   /**
    * The primary font. Used in most places.
    */
-  primary: fonts.spaceGrotesk,
+  primary: fonts.Montserrat,
   /**
    * An alternate font used for perhaps titles and stuff.
    */
-  secondary: Platform.select({ ios: fonts.helveticaNeue, android: fonts.sansSerif }),
-  /**
-   * Lets get fancy with a monospace font!
-   */
-  code: Platform.select({ ios: fonts.courier, android: fonts.monospace }),
+  secondary: fonts.Inter,
 }
