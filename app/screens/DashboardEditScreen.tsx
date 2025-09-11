fd0b235b98aa7745e8c21320eab2c8ca256403c0
ekansnek <mrekenchen@gmail.com>
2025-09-11 14:37:24 +1000

    feat(dashboard): :sparkles: added back button for edit dashboard screen

--- app/screens/DashboardEditScreen.tsx
+++ app/screens/DashboardEditScreen.tsx
@@ -1,14 +1,27 @@
 // placeholder blank screen for now
 import { FC } from "react"
+import { Pressable } from "react-native"
+import { View } from "react-native"
 
+import { Icon } from "@/components/Icon"
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import { AppStackScreenProps } from "@/navigators/AppNavigator"
+import { useAppTheme } from "@/theme/context"
+import { $topRightIcons, $headerIcons } from "@/theme/styles"
 
 export const DashboardEditScreen: FC<AppStackScreenProps<"EditDashboard">> =
-  function DashboardEditScreen() {
+  function DashboardEditScreen(_props) {
+    const { navigation } = _props
+    const { themed } = useAppTheme()
+
     return (
       <Screen preset="scroll" safeAreaEdges={["top"]}>
+        <View style={themed($topRightIcons)}>
+          <Pressable onPress={() => navigation.goBack()} style={themed($headerIcons)}>
+            <Icon icon="anchor" />
+          </Pressable>
+        </View>
         <Text preset="heading">Dashboard Editor Screen</Text>
       </Screen>
     )
