c1296562eb40d821b8fe860b54ef5281aa7151f2
Aaron Lai <aklai@student.unimelb.edu.au>
2025-09-13 13:48:38 +1000

    feat(general): :sparkles: create month selector bar component

---
+++ app/components/DateSelectorBar.tsx
@@ -0,0 +1,62 @@
+import { Pressable } from "react-native"
+import { format, addMonths, addDays } from "date-fns"
+import { useTheme, XStack, Text } from "tamagui"
+
+import { Icon } from "./Icon"
+
+type SelectorMode = "month" | "day"
+
+interface DateSelectorBarProps {
+  mode: SelectorMode
+  selectedDate: Date
+  setSelectedDate: (date: Date) => void
+}
+
+const DateSelectorBar = ({ mode, selectedDate, setSelectedDate }: DateSelectorBarProps) => {
+  const theme = useTheme()
+
+  const adjustDate = (direction: "next" | "previous") => {
+    const adjustment = direction === "next" ? 1 : -1
+    const adjustFn = mode === "month" ? addMonths : addDays
+    setSelectedDate(adjustFn(selectedDate, adjustment))
+  }
+
+  const displayDate = format(selectedDate, mode === "month" ? "MMMM yyyy" : "E, d MMM")
+
+  return (
+    <XStack
+      height={56}
+      width="100%"
+      zIndex={1}
+      alignItems="center"
+      justifyContent="space-between"
+      boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.4)"}
+      px={16}
+    >
+      {/* Slider icon for configurations */}
+      <Pressable>
+        <Icon icon="sliders" size={24} color={theme.mono900.val} />
+      </Pressable>
+
+      {/* Month increment/decrement selector */}
+      <XStack gap={4} alignItems="center">
+        <Pressable onPress={() => adjustDate("previous")}>
+          <Icon icon="left" size={24} color={theme.mono900.val} />
+        </Pressable>
+        <Text fontSize={16} fontWeight="600" marginHorizontal={10}>
+          {displayDate}
+        </Text>
+        <Pressable onPress={() => adjustDate("next")}>
+          <Icon icon="right" size={24} color={theme.mono900.val} />
+        </Pressable>
+      </XStack>
+
+      {/* Search icon - disabled, not in use */}
+      <Pressable disabled={true}>
+        <Icon icon="search" size={24} color={theme.mono900.val} />
+      </Pressable>
+    </XStack>
+  )
+}
+
+export { DateSelectorBar }
