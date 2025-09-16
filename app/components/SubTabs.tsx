740afa0a0d463d0c29e8da9c63fcc5311dc7b24d
angchn <angelaisuychen@gmail.com>
2025-09-16 23:43:53 +1000

    feat(my-roster): :sparkles: added selected state styling to the buttons

--- app/components/SubTabs.tsx
+++ app/components/SubTabs.tsx
@@ -1,5 +1,5 @@
 import { GestureResponderEvent } from "react-native"
-import { Button, XStack } from "tamagui"
+import { Button, styled, XStack, YStack } from "tamagui"
 
 import { HeaderText } from "./HeaderText"
 
@@ -14,6 +14,24 @@ interface SubTabsProps {
   onTabChange: (key: string) => void
 }
 
+const TabButton = styled(Button, {
+  flex: 1,
+  borderRadius: 0,
+  chromeless: true,
+  padding: 0,
+  position: "relative",
+  paddingVertical: 0,
+  minHeight: "unset",
+  alignItems: "center",
+  justifyContent: "center",
+  borderWidth: 0,
+  borderColor: "transparent",
+
+  pressStyle: {
+    backgroundColor: "$primary700",
+  },
+})
+
 export const SubTabs = ({ tabs, activeTab, onTabChange }: SubTabsProps) => {
   return (
     <XStack
@@ -25,24 +43,29 @@ export const SubTabs = ({ tabs, activeTab, onTabChange }: SubTabsProps) => {
       borderBottomRightRadius="$radius.6"
       backgroundColor="$primary500"
       overflow="hidden"
+      borderColor="transparent"
     >
       {tabs.map((tab) => {
         const selected = tab.key === activeTab
-        const onPress = (e: GestureResponderEvent) => onTabChange?.(tab.key)
+        const onPress = (_e: GestureResponderEvent) => onTabChange?.(tab.key)
         return (
-          <Button
-            key={tab.key}
-            onPress={onPress}
-            aria-selected={selected}
-            flex={1}
-            borderRadius={0}
-            chromeless
-            padding={0}
-          >
-            <HeaderText variant="h3" color="$white100">
-              {tab.label}
-            </HeaderText>
-          </Button>
+          <TabButton key={tab.key} onPress={onPress} aria-selected={selected}>
+            <YStack flex={1} alignItems="center" justifyContent="center" width="100%" height="100%">
+              {selected && (
+                <XStack
+                  position="absolute"
+                  top={0}
+                  height={3}
+                  width="100%"
+                  backgroundColor="$accent500"
+                  borderColor="$accent500"
+                />
+              )}
+              <HeaderText variant="h3" color={selected ? "$accent500" : "$white100"}>
+                {tab.label}
+              </HeaderText>
+            </YStack>
+          </TabButton>
         )
       })}
     </XStack>
