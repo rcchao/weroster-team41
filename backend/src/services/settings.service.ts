d5eb14abfa98997840eb5d7757a9c02cf9b629b3
Bec Chao <becchao@gmail.com>
2025-09-21 20:26:54 +1000

    feat(api): add fetch api for user settings

---
+++ backend/src/services/settings.service.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client"
+import { DashboardPreferences } from "../../../backend/src/types/settings.types"
+
+export class SettingsService {
+  constructor(private prisma: PrismaClient) {}
+
+  async getDashboardPreferences(userId: number): Promise<DashboardPreferences> {
+    const dashboardPreferences = await this.prisma.dashboardPreference.findUnique({
+      where: { userId: userId },
+    })
+
+    if (!dashboardPreferences) {
+      throw new Error("dashboardPreferences not found")
+    }
+
+    return dashboardPreferences
+  }
+}
