96180c7dcad349bbc046a9f0ecf2e5b7cc200960
Aaron Lai <aklai@student.unimelb.edu.au>
2025-09-23 18:41:58 +1000

    refactor(api): :label: add eventdata type

---
+++ backend/src/types/event.types.ts
@@ -0,0 +1,25 @@
+import { Prisma } from ".prisma/client"
+
+export type ShiftDetails = Prisma.EventGetPayload<{
+  select: {
+    id: true
+    start_time: true
+    end_time: true
+    on_call: true
+    activity: true
+    location: true
+    eventAssignments: {
+      select: {
+        id: true
+        user: {
+          select: {
+            id: true
+            first_name: true
+            last_name: true
+          }
+        }
+        designation: true
+      }
+    }
+  }
+}>
