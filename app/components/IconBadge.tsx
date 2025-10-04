274ccf2d43361543f8cee21850b5b86482d2b868
angchn <angelaisuychen@gmail.com>
2025-10-04 13:04:34 +1000

    feat(dashboard): :sparkles: create new DashboardCard component

---
+++ app/components/IconBadge.tsx
@@ -0,0 +1,53 @@
+import { Circle } from "tamagui"
+
+import { Icon, type IconTypes } from "./Icon"
+
+type IconBadgeConfig = {
+  icon: IconTypes
+  bgColor: string
+  color: string
+}
+
+const ICON_BADGE_CONFIG: Record<string, IconBadgeConfig> = {
+  AMShift: {
+    icon: "am",
+    bgColor: "yellow300",
+    color: "yellow800",
+  },
+  PMShift: {
+    icon: "pm",
+    bgColor: "red300",
+    color: "red800",
+  },
+  AHShift: {
+    icon: "afterHours",
+    bgColor: "secondary300",
+    color: "secondary800",
+  },
+  halfDay: {
+    icon: "circleSlash",
+    bgColor: "secondary400",
+    color: "secondary800",
+  },
+  fullDay: {
+    icon: "circle",
+    bgColor: "yellow400",
+    color: "yellow800",
+  },
+}
+
+interface IconBadgeProps {
+  type: "AMShift" | "PMShift" | "AHShift" | "halfDay" | "fullDay"
+}
+
+export const IconBadge = ({ type }: IconBadgeProps) => {
+  const iconConfig = ICON_BADGE_CONFIG[type]
+
+  const icon = iconConfig.icon
+  //   const bgColor = iconConfig.bgColor
+  return (
+    <Circle size="$4" backgroundColor="$yellow400">
+      <Icon icon={icon} />
+    </Circle>
+  )
+}
