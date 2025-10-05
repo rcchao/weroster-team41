c5f26bab95aefe5538b61ebb44efd9527068fc65
angchn <angelaisuychen@gmail.com>
2025-10-05 22:56:38 +1100

    feat(my-roster): :sparkles: create a Session type

--- app/components/ShiftDetailsSubheader.tsx
+++ app/components/ShiftDetailsSubheader.tsx
@@ -4,27 +4,33 @@ import { useTheme, XStack } from "tamagui"
 import { BodyText } from "./BodyText"
 import { Icon } from "./Icon"
 
-const SESSION_COLOUR_MAP: Record<string, string> = {
+const SESSION_LABEL_BG_COLOUR_MAP: Record<Session, string> = {
   AM: "yellow300",
   PM: "red300",
   AH: "secondary300",
 }
 
+export type Session = "AM" | "PM" | "AH"
+
 interface ShiftDetailsSubheaderProps {
-  start: Date
-  end: Date
-  session: string
+  startDate: Date
+  endDate: Date
+  session: Session
 }
 
-export const ShiftDetailsSubheader = ({ start, end, session }: ShiftDetailsSubheaderProps) => {
+export const ShiftDetailsSubheader = ({
+  startDate,
+  endDate,
+  session,
+}: ShiftDetailsSubheaderProps) => {
   const theme = useTheme()
 
-  const displayDate = format(start, "EEE, d MMM")
-  const timeRange = `${format(start, "HH:mm")} - ${format(end, "HH:mm")}`
-  const duration = differenceInHours(end, start)
+  const displayDate = format(startDate, "EEE, d MMM")
+  const timeRange = `${format(startDate, "HH:mm")} - ${format(endDate, "HH:mm")}`
+  const duration = differenceInHours(endDate, startDate)
 
   const dateIcon = session === "AM" || session === "PM" ? "sun" : "moon"
-  const bgColor = theme[SESSION_COLOUR_MAP[session]]?.val ?? theme.white100.val
+  const bgColor = theme[SESSION_LABEL_BG_COLOUR_MAP[session]]?.val ?? theme.white100.val
 
   return (
     <XStack backgroundColor="$white500" width="100%" justifyContent="space-between" padding={16}>
