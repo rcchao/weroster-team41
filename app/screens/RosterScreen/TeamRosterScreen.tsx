27a84687a55e2e078325beaff0ea2a61757ec122
Aaron Lai <aklai@student.unimelb.edu.au>
2025-10-10 12:16:12 +1100

    refactor(open-shifts): :recycle: display roster calendar on open shift tab

--- app/screens/RosterScreen/TeamRosterScreen.tsx
+++ app/screens/RosterScreen/TeamRosterScreen.tsx
@@ -3,10 +3,12 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack"
 
 import { DateSelectorBar } from "@/components/DateSelectorBar"
 import type { Session } from "@/components/ShiftDetailsSubheader"
+import { SubmitButton } from "@/components/SubmitButton"
 import { TeamRosterShiftTabBar } from "@/components/TeamRosterShiftTabBar"
 import { TeamRosterTableView } from "@/components/TeamRosterTableView"
 import type { RosterStackParamList } from "@/navigators/DashboardNavigator"
 import { useTeamShifts } from "@/services/hooks/useTeamShifts"
+import { usePostAssignmentRequest } from "@/services/hooks/useUserRequests"
 
 type Props = NativeStackScreenProps<RosterStackParamList, "TeamRoster">
 
@@ -16,11 +18,32 @@ export function TeamRosterScreen(_props: Props) {
 
   const { teamShifts } = useTeamShifts(selectedDate, session)
 
+  const mutation = usePostAssignmentRequest()
+
+  const postOpenShiftRequest = async () => {
+    const assignmentRequest = { event_id: 4 }
+    try {
+      const data = await mutation.mutateAsync(assignmentRequest)
+      if (data.success) {
+        console.log("Posted successfully", data)
+      } else {
+        // Error messages here are "Not an open shift, cannot make assignment request"
+        // and "Event not found". If either of these are the case, the frontend
+        // shouldn't even allow a user to apply to the open shift so don't
+        // worry about bubbling these errors up to the frontend
+        console.log("Post failed", data.error)
+      }
+    } catch (error) {
+      console.error("Error posting:", error)
+    }
+  }
+
   return (
     <>
       <DateSelectorBar mode="day" selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
       <TeamRosterShiftTabBar timeSection={session} setTimeSection={setSession} />
       {teamShifts && <TeamRosterTableView campusShifts={teamShifts} />}
+      <SubmitButton text="apply for open shift" onPress={postOpenShiftRequest} />
     </>
   )
 }
