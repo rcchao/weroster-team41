a5cca66bb3d7e6b23badf5fd1d75241e7833113c
angchn <angelaisuychen@gmail.com>
2025-10-10 13:11:12 +1100

    feat(my-roster): :sparkles: add a dialog to shift card to display shift details

--- app/components/ShiftDetailCard.tsx
+++ app/components/ShiftDetailCard.tsx
@@ -121,6 +121,7 @@ const ShiftDetailCard = ({ shift }: ShiftDetailCardProps) => {
       shadowRadius={8}
       borderRadius="$radius.4"
       padding="$5"
+      alignItems="center"
     >
       <YStack gap="$4">
         <ShiftHeader location={shift.location} address={campus?.address} />
