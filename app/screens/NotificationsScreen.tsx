b5225292823bef3298ff466f0d1dfc5d077d37c8
angchn <angelaisuychen@gmail.com>
2025-10-16 23:36:39 +1100

    feat(notifications): :sparkles: display SwapDetailCard component in modal

--- app/screens/NotificationsScreen.tsx
+++ app/screens/NotificationsScreen.tsx
@@ -3,7 +3,12 @@ import { YStack, Spinner, ScrollView } from "tamagui"
 
 import { BackHeader } from "@/components/BackHeader"
 import { BodyText } from "@/components/BodyText"
-import { InteractiveNotification, Notification } from "@/components/Notification"
+import {
+  InteractiveNotification,
+  Notification,
+  NotificationStatusType,
+  SwapStatusType,
+} from "@/components/Notification"
 import { Screen } from "@/components/Screen"
 import type { AppStackScreenProps } from "@/navigators/AppNavigator"
 import { useUserNotifications } from "@/services/hooks/useUserNotifications"
@@ -21,7 +26,6 @@ export const NotificationsScreen: FC<NotificationsScreenProps> = function Notifi
     <Screen safeAreaEdges={["top"]} contentContainerStyle={$styles.barContainer}>
       <BackHeader title="Notifications" navigation={navigation} />
       <ScrollView>
-        <BodyText variant="body3">{JSON.stringify(userNotifications)}</BodyText>
         <YStack paddingVertical="$4">
           {isPending ? (
             <YStack paddingTop="60%" gap="$3" alignItems="center">
@@ -31,18 +35,10 @@ export const NotificationsScreen: FC<NotificationsScreenProps> = function Notifi
           ) : userNotifications && userNotifications.length > 0 ? (
             userNotifications.map((notifs, idx) => (
               <YStack key={idx} justifyContent="center">
-                {/* <BodyText>
-                  {" "}
-                  This is a {String(notifs.notif_type)} notification. {"\n"} This notification is
-                  timestamped at {format(notifs.created_at, "dd/MM/yy - HH:mm")}. {"\n"} This
-                  notification is {notifs.is_read ? "read" : "unread"} and{" "}
-                  {notifs.requires_action ? "requires action" : "does not require an action"}. This
-                  notification has the status {String(notifs.status)}.
-                </BodyText> */}
                 {notifs.notif_type === "SWAP" ? (
                   <InteractiveNotification
                     requestType={notifs.notif_type}
-                    statusType={notifs.status}
+                    statusType={notifs.status as SwapStatusType}
                     notificationDate={notifs.created_at}
                     fromUserFirstName={notifs.first_name || ""}
                     fromUserLastName={notifs.last_name || ""}
@@ -50,28 +46,28 @@ export const NotificationsScreen: FC<NotificationsScreenProps> = function Notifi
                     isRead={notifs.is_read}
                     requiresAction={notifs.requires_action}
                     shift={notifs.event}
+                    message={notifs.message}
                   />
                 ) : notifs.notif_type === "LEAVE" ? (
                   <Notification
                     requestType={notifs.notif_type}
-                    statusType={notifs.status}
+                    statusType={notifs.status as NotificationStatusType}
                     notificationDate={notifs.created_at}
                     eventDate={notifs.leave_start_date}
                     isRead={notifs.is_read}
-                    requiresAction={notifs.requireAction}
+                    requiresAction={notifs.requires_action}
                   />
                 ) : notifs.notif_type === "ASSIGNMENT" ? (
                   <Notification
                     requestType={notifs.notif_type}
-                    statusType={notifs.status}
+                    statusType={notifs.status as NotificationStatusType}
                     notificationDate={notifs.created_at}
                     eventDate={notifs.assignmentRequest_start_date}
                     eventLocation={notifs.location}
                     isRead={notifs.is_read}
-                    requiresAction={notifs.requireAction}
+                    requiresAction={notifs.requires_action}
                   />
                 ) : null}
-                {console.log(idx, notifs)}
               </YStack>
             ))
           ) : (
