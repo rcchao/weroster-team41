3dc1b03d5e29dd0224198982f252b5229d4f0455
Aaron Lai <aklai@student.unimelb.edu.au>
2025-10-25 21:36:06 +1100

    refactor(general): :recycle: include swagger api-docs as a route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import express from "express"
 import cors from "cors"
 import { PrismaClient } from "@prisma/client"
 import routes from "./routes"
+import { setupSwagger } from "./swagger"
 
 const app = express()
 const prisma = new PrismaClient()
@@ -13,6 +14,8 @@ app.use(express.json())
 app.locals.prisma = prisma
 
 app.use("/api", routes)
+
+setupSwagger(app)
 
 app.listen(3000, () => {
   console.log("Server running on port 3000")
