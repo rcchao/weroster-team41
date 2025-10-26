2591fc27855b703d8d92fe77dd9a1ebd8ef6ec1c
Aaron Lai <aklai@student.unimelb.edu.au>
2025-10-26 12:32:37 +1100

    feat(team-roster): :sparkles: show default message for campus with no locations

--- app/components/TeamRosterTableView.tsx
+++ app/components/TeamRosterTableView.tsx
@@ -18,65 +18,72 @@ const TeamRosterTableView = ({ campusShifts }: TeamRosterTableViewProps) => {
   const accordionSections = campusShifts.map((campus) => ({
     sectionText: campus.name,
     isCurrent: false,
-    children: (
-      <>
-        {campus.locations.map((location, index) => (
-          <YStack key={location.id}>
-            {index > 0 && <Separator borderColor={borderColor} />}
+    children:
+      campus.locations.length === 0 ? (
+        <YStack padding="$3" backgroundColor="$white100">
+          <BodyText variant="body3" color="$mono500" textAlign="center">
+            No locations with events at this time
+          </BodyText>
+        </YStack>
+      ) : (
+        <>
+          {campus.locations.map((location, index) => (
+            <YStack key={location.id}>
+              {index > 0 && <Separator borderColor={borderColor} />}
 
-            <XStack>
-              {/* Location Column */}
-              <YStack
-                width={120}
-                padding="$3"
-                backgroundColor="$white100"
-                justifyContent="center"
-                borderRightWidth={borderWidth}
-                borderRightColor={borderColor}
-              >
-                <BodyText variant="body2" fontWeight="800" textAlign="center">
-                  {location.name}
-                </BodyText>
-              </YStack>
-
-              {/* Events Column */}
-              <YStack flex={1} padding="$3" justifyContent="center" backgroundColor="$white100">
-                {location.events.length === 0 ? (
-                  <BodyText variant="body4" color="$mono500">
-                    No shifts scheduled
+              <XStack>
+                {/* Location Column */}
+                <YStack
+                  width={120}
+                  padding="$3"
+                  backgroundColor="$white100"
+                  justifyContent="center"
+                  borderRightWidth={borderWidth}
+                  borderRightColor={borderColor}
+                >
+                  <BodyText variant="body2" fontWeight="800" textAlign="center">
+                    {location.name}
                   </BodyText>
-                ) : (
-                  <>
-                    {location.events.map((event) => (
-                      <YStack key={event.id} gap="$1.5">
-                        {/* Activity Header */}
-                        <XStack gap="$2" alignItems="center" marginBottom="$1.5">
-                          <Icon icon="stethoscope" size={16} />
-                          <BodyText variant="body2" fontWeight="800">
-                            {event.activity || "Unassigned"}
-                            {event.on_call && " [OC]"}
-                          </BodyText>
-                        </XStack>
+                </YStack>
 
-                        {/* Assigned Users */}
-                        {event.eventAssignments.map((assignment) => (
-                          <XStack key={assignment.id} gap="$2" alignItems="center">
-                            <Icon icon="user2" size={16} />
-                            <BodyText variant="body3">
-                              {assignment.first_name} {assignment.last_name}
+                {/* Events Column */}
+                <YStack flex={1} padding="$3" justifyContent="center" backgroundColor="$white100">
+                  {location.events.length === 0 ? (
+                    <BodyText variant="body4" color="$mono500">
+                      No shifts scheduled
+                    </BodyText>
+                  ) : (
+                    <>
+                      {location.events.map((event) => (
+                        <YStack key={event.id} gap="$1.5">
+                          {/* Activity Header */}
+                          <XStack gap="$2" alignItems="center" marginBottom="$1.5">
+                            <Icon icon="stethoscope" size={16} />
+                            <BodyText variant="body2" fontWeight="800">
+                              {event.activity || "Unassigned"}
+                              {event.on_call && " [OC]"}
                             </BodyText>
                           </XStack>
-                        ))}
-                      </YStack>
-                    ))}
-                  </>
-                )}
-              </YStack>
-            </XStack>
-          </YStack>
-        ))}
-      </>
-    ),
+
+                          {/* Assigned Users */}
+                          {event.eventAssignments.map((assignment) => (
+                            <XStack key={assignment.id} gap="$2" alignItems="center">
+                              <Icon icon="user2" size={16} />
+                              <BodyText variant="body3">
+                                {assignment.first_name} {assignment.last_name}
+                              </BodyText>
+                            </XStack>
+                          ))}
+                        </YStack>
+                      ))}
+                    </>
+                  )}
+                </YStack>
+              </XStack>
+            </YStack>
+          ))}
+        </>
+      ),
   }))
 
   return (
